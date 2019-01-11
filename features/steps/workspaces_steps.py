354432457a2e731d19a7288cd38c096e04849409
vzamorano <vivianzamoranocastelo58@gmail.com>
2019-01-11 00:15:04 -0400

    finalupdate

--- features/steps/workspaces_steps.py
+++ features/steps/workspaces_steps.py
@@ -1,4 +1,4 @@
-#import time
+# import time
 
 from behave import Given, then, step
 
@@ -14,11 +14,29 @@ def create_workspaces_step(context):
     context.workspaces_status, context.project_response = workspaces_services.create_workspace(data)
 
 
+@step("I get a workspace")
+def get_workspaces_step(context):
+    workspaces_services.get_workspace(id=context.workspace_response["id"])
 
-#@then(u'I verify workspaces creation status is {status_code}')
-#def step_impl(context, status_code):
- #   print(context.workspaces_status)
-  #  assert context.workspaces_status == status_code, "Workspaces creation status is %s" % status_code
+
+@step("I update the workspace")
+def update_workspaces_step(context):
+    data = {}
+    for row in context.table:
+        data = {"name": str(row['name'])}
+    context.workspace_status, context.workspace_response = workspaces_services.update_workspace(
+        id=str(context.workspace_response["id"]), data=data)
+
+
+@step("I delete the workspace")
+def update_workspaces_step(context):
+    context.workspace_status = workspaces_services.delete_workspace(id=str(context.workspace_response["id"]))
+
+
+# @then(u'I verify workspaces creation status is {status_code}')
+# def step_impl(context, status_code):
+#   print(context.workspaces_status)
+#  assert context.workspaces_status == status_code, "Workspaces creation status is %s" % status_code
 
 
 @step(u'I verify workspaces schema')
--- features/workspaces.feature
+++ features/workspaces.feature
@@ -6,3 +6,30 @@ Feature: showing off behave
         | name |
     Then I verify workspaces creation status is 200
     And I verify workspaces schema
+
+
+    Scenario: Get workspace
+    Given I create a workspace
+      | name            |
+      | Test Workspace
+    When I get a workspace
+#    Then I verify workspace get status is 200
+    And I verify workspace schema
+
+
+  Scenario: Update the workspace
+    Given I create a workspace
+    | name          |
+     | First workspace |
+    When I update the workspace
+     | name           |
+    | Update workspace |
+    Then I verify workspace updated status is 200
+    And I verify workspace schema
+
+  Scenario: Delete the workspace
+   Given I create a workspace
+    | name          |
+    | First workspace |
+    When I delete the workspace
+    Then I verify workspace deleted status is 204
